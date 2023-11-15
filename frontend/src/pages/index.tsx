664f46a1c73dbb44e0776d8605ccbcd55489c12b
DM2V <marcelo.malte11@gmail.com>
2023-11-14 19:18:28 -0500

    Add campus page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,5 @@
+import Campus from "../pages/Public/Campus";
+
 export default function Home() {
-  return (
-    <div className="bg-black">
-      <h1 className="text-red-700">The Flash DM2V</h1>
-    </div>
-  );
+  return <Campus />;
 }
