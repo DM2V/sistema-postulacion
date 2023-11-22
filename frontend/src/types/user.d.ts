f2675ae1231f8351e87d79ab45761c4866cf098a
Dayse <dnpoma@espe.edu.ec>
2023-11-22 16:01:26 -0500

    Update Data Base

--- frontend/src/types/user.d.ts
+++ frontend/src/types/user.d.ts
@@ -1,10 +1,9 @@
 export interface user {
-    username: string;
-    email: string;
-    name?: string;
-    password?: string;
-    name?: string;
-    avatar?: string;
-    role?: string;
-  }
-  
+  username: string;
+  email: string;
+  name?: string;
+  password?: string;
+  name?: string;
+  avatar?: string;
+  role?: string;
+}
