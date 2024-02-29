3135cd3a7b5ee57af8f6e629de83f082c75a5818
Marcelo Malte <marcelo.malte11@gmail.com>
2024-02-29 18:51:42 -0500

    Update PocketBase configuration and add image link function

--- frontend/src/utils/pocketbase.ts
+++ frontend/src/utils/pocketbase.ts
@@ -1,3 +1,9 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const BACKEND_ADDRESS = "http://127.0.0.1:8090"
+
+export const pb = new PocketBase(BACKEND_ADDRESS);
+
+export function getLinkForImage(imageName: string, collection: string, recordId: string) {
+    return `${BACKEND_ADDRESS}/api/files/${collection}/${recordId}/${imageName}`
+}
